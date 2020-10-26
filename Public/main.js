d838753e6c257c940a4f836a2b71d222af30120d
mihai.g0g05arui <mihai.indreias@gmail.com>
2020-10-26 09:35:47 +0200

    ok now

--- Public/main.js
+++ Public/main.js
@@ -1,21 +1,20 @@
-/** 
+/**
  * Parte din .html / Nu din node
-*/
+ */
 
 function Subs() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/subscribe?mail=" + document.getElementById("Email").value +"&difficulty=medium", false);
-    xhr.send(null);
-};
-function FirstSub()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/clickedSubscribe", false);
-    xhr.send(null);
+	var xhr = new XMLHttpRequest();
+	xhr.open(
+		"GET",
+		"/subscribe?mail=" +
+			document.getElementById("Email").value +
+			"&difficulty=medium",
+		false
+	);
+	xhr.send(null);
 }
-function Visits()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/visit", false);
-    xhr.send(null);
+function FirstSub() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "/clickedSubscribe", false);
+	xhr.send(null);
 }
